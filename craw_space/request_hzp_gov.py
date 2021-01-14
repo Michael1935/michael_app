375177c0135c7d9e87f82101709f490a44361294
wangyong315 <291140738@qq.com>
2021-01-14 21:06:07 +0800

    新增gitignore

--- craw_space/request_hzp_gov.py
+++ craw_space/request_hzp_gov.py
@@ -5,19 +5,22 @@ import json
 
 if __name__ == '__main__':
     url = 'http://scxk.nmpa.gov.cn:81/xk/itownet/portalAction.do?method=getXkzsList'
-    data = {
-        'on': 'true',
-        'page': '1',
-        'pageSize': '15',
-        'productName':'',
-        'conditionType': '1',
-        'applyname':''
-    }
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
     }
 
-    json_ids = requests.post(url=url, data=data, headers=headers).json()
+    for id in range(1, 6):
+        page = str(id)
+        data = {
+            'on': 'true',
+            'page': page,
+            'pageSize': '15',
+            'productName':'',
+            'conditionType': '1',
+            'applyname':''
+        }
+
+        json_ids = requests.post(url=url, data=data, headers=headers).json()
     id_list = []
     for dic in json_ids['list']:
         id_list.append(dic['ID'])
